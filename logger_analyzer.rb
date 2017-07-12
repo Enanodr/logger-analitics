c693ca2860e486749614f3a7db0a37d34f31b84c
danielaRiesgo <danielap.riesgo@gmail.com>
2017-07-12 16:59:23 -0300

    Adding csv saving of each message

--- logger_analyzer.rb
+++ logger_analyzer.rb
@@ -5,7 +5,19 @@ require 'csv'
 require 'byebug'
 
 channels = Dir.glob('./slacks/*').map { |dir|  dir.split('/').last }
+
+# la estructura de slacks_by_channel va a ser
+# {
+#   "CHANNEL_ID" => {
+#     "WOLOXER_ID" => [{
+#                       "text": context of message,
+#                       "time": seconds,
+#                       "month": "YYYY-MM"
+#                     }, ...],
+#     "WOLOXER_ID" => ...,
+# }
 slacks_by_channel = { }
+
 # Define an array entry by channel in a hash
 channels.each { |channel| slacks_by_channel[channel] = [] }
 
@@ -40,9 +52,7 @@ end
    woloxers = woloxers.uniq
  end
 
-
-
-# la estrictira va a ser
+# la estructura va a ser
 # {
 #   "WOLOXER_ID" => {
 #     "MONTH" => "ALL THE TEXT FROM THIS MONTH"
@@ -50,24 +60,38 @@ end
 #   },
 #   WOLOXER_ID => ...
 # }
-woloxers_slacks_by_month = { }
-woloxers.each do |woloxer|
-  woloxers_slacks_by_month[woloxer] = { }
-  slacks_by_channel.each  do |_, data|
-    next unless data[woloxer]
-    data[woloxer].each { |slack| woloxers_slacks_by_month[woloxer][slack['month']] = [] }
-  end
-  slacks_by_channel.each do |_, data|
-    next unless data[woloxer]
-    data[woloxer].each { |slack| woloxers_slacks_by_month[woloxer][slack['month']] << slack['text'] }
-    # woloxers_slacks_by_month[woloxer][slack['month']] = woloxers_slacks_by_month[woloxer][slack['month']]
+# woloxers_slacks_by_month = { }
+# woloxers.each do |woloxer|
+#   woloxers_slacks_by_month[woloxer] = { }
+#   slacks_by_channel.each  do |_, data|
+#     next unless data[woloxer]
+#     data[woloxer].each { |slack| woloxers_slacks_by_month[woloxer][slack['month']] = [] }
+#   end
+#   slacks_by_channel.each do |_, data|
+#     next unless data[woloxer]
+#     data[woloxer].each { |slack| woloxers_slacks_by_month[woloxer][slack['month']] << slack['text'] }
+#   end
+# end
+
+#slacks_by_channel_by_woloxer
+# CSV Headers
+csv_array = [['woloxer_id', 'channel_id', 'month', 'timestamp', 'message']]
+# generate CSV array
+slacks_by_channel.each do |channel_id, data|
+  data.each do |user_id, slacks|
+    slacks.each do |slack|
+      csv_array << [user_id, channel_id, slack['month'], slack['time'], slack['text']]
+    end
   end
 end
+# Save CSV File
+CSV.open("slacks_by_channel_by_woloxer.csv", "w") { |csv| csv_array.each { |line| csv << line } }
 
-byebug
+
+
 # CSV Headers
 # 'woloxer_id', 'month', 'amount_of_messages', 'sentiment'
-csv_array = [['woloxer_id', 'month', 'amount_of_messages', 'sentiment']]
+# csv_array = [['woloxer_id', 'month', 'amount_of_messages', 'sentiment']]
 
 # generate CSV array
 # woloxers_slacks_by_month.each do |woloxer, monthly_data|
