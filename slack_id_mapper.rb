3c881068bfe9aef07bc6c1eaae88a5033286b42c
Enanodr <diego.raffo@wolox.com.ar>
2017-07-12 14:47:55 -0300

    remove keys

--- slack_id_mapper.rb
+++ slack_id_mapper.rb
@@ -1,5 +1,6 @@
 include 'httparty'
 
-slack_url = 'https://slack.com/api/users.list?token=xoxp-2516879529-21435805462-211884793124-0697cd757818d22120fff1d3bf3fe23e'
+token = "token=#{ENV[SLACK_ID]}"
+slack_url = 'https://slack.com/api/users.list?' + token
 response = HTTParty.get slack_url
 parsed_response = JSON.parse response.body
