7a4b74f19add733f6ab40afc44a364380bfce23e
Enanodr <diego.raffo@wolox.com.ar>
2017-07-13 16:10:02 -0300

    slack query for emails. Corrected csv with emails instead of slack ids

--- slack_id_mapper.rb
+++ slack_id_mapper.rb
@@ -1,6 +1,14 @@
-include 'httparty'
+require 'httparty'
+require 'byebug'
 
-token = "token=#{ENV[SLACK_ID]}"
-slack_url = 'https://slack.com/api/users.list?' + token
+
+slack_url = 'https://slack.com/api/users.list?token=' + ENV['SLACK_TOKEN']
 response = HTTParty.get slack_url
 parsed_response = JSON.parse response.body
+
+email_by_slack_id = { }
+parsed_response['members'].each do |member|
+  email_by_slack_id[member['id']] = member['profile']['email']
+end
+
+CSV.open("emails_by_slack_id.csv", "w") { |csv| email_by_slack_id.each { |id, email| csv << [id, email] } }
